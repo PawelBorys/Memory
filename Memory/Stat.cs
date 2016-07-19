fd355fbdbc398dafdff95c1dc671b2a18102c7de
Syrob <pawel13bor@gmail.com>
2016-07-19 22:47:35 +0200

    created db model & dbcontext

---
+++ Memory/Stat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memory
+{
+    class Stat
+    {
+        public string name { get; set; }
+        public int clicks { get; set; }
+        public TimeSpan time { get; set; }
+    }
+
+    public class StatsContext : DbContext
+    {
+        public virtual DbSet<Stat> fours { get; set; }
+        public virtual DbSet<Stat> fives { get; set; }
+    }
+}
